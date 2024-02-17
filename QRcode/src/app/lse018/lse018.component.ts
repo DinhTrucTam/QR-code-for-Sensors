53e665a2ab09969c7b864523ed080d4f700e8d52
DinhTrucTam <tructam2992@gmail.com>
2024-02-17 21:21:00 +0700

    Initial commit msg

---
+++ QRcode/src/app/lse018/lse018.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lse018',
+  templateUrl: './lse018.component.html',
+  styleUrls: ['./lse018.component.css']
+})
+export class Lse018Component {
+
+}
---
+++ QRcode/src/app/lsn50v2s31b/lsn50v2s31b.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lsn50v2s31b',
+  templateUrl: './lsn50v2s31b.component.html',
+  styleUrls: ['./lsn50v2s31b.component.css']
+})
+export class Lsn50v2s31bComponent {
+
+}
